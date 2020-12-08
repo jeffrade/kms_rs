c95e798a9d29bbb20d8f3bc05c1f4f2715faecc6
Jeff Rade <jeffrade@gmail.com>
2020-12-07 20:36:38 -0600

    Add list-keys request and parsing for client CMK's

--- README.md
+++ README.md
@@ -4,6 +4,12 @@
 
 _Disclaimer: This project has not been audited and not recommended for production environments._
 
+:warning: This project is under construction! :warning: 
+
 Uses [rusoto](https://github.com/rusoto/rusoto).
 
-:warning: This project is under construction! :warning: 
+See [examples](https://github.com/jeffrade/kms_rs/blob/master/examples/src/main.rs) for usage.
+
+#### As functionality is added, it will be listed below.
+
+ - Retrieve a list of all CMK's (Customer Master Keys) in region us-east-1
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,68 @@
-//! A create for using AWS KMS
+//! A crate for using AWS KMS
 
+extern crate rusoto_core;
+
+use rusoto_core::Region;
+use rusoto_kms::{KeyListEntry, Kms, KmsClient, ListKeysRequest, ListKeysResponse};
+use tokio::runtime::Runtime;
+
+/// Gets the list of all Customer Master Keys (CMKs) in current AWS account (defaults to us-east-1).
 pub fn list_keys() -> Vec<String> {
-    vec![String::from("TODO")]
+    Runtime::new()
+        .expect("Failed to create Tokio runtime")
+        .block_on(get_keys(get_client()))
+}
+
+async fn get_keys(client: KmsClient) -> Vec<String> {
+    let request = ListKeysRequest::default();
+
+    let response: ListKeysResponse = client.list_keys(request).await.unwrap();
+    parse_key_list_entries(response.keys.unwrap_or_default())
+}
+
+fn get_client() -> KmsClient {
+    KmsClient::new(Region::UsEast1)
+}
+
+fn parse_key_list_entries(key_list: Vec<KeyListEntry>) -> Vec<String> {
+    let mut keys_str: Vec<String> = Vec::new();
+    for key in key_list {
+        let mut key_str: String = String::from(&key.key_arn.unwrap_or_default());
+        key_str.push('|');
+        key_str.push_str(&key.key_id.unwrap_or_default());
+        keys_str.push(key_str);
+    }
+
+    keys_str
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_parse_key_list_entries() {
+        let mut mock_key_list: Vec<KeyListEntry> = Vec::new();
+        let first_key = KeyListEntry {
+            key_arn: Some(String::from(
+                "arn:aws:kms:us-east-1:123456789:key/abcd-4321-wxyz",
+            )),
+            key_id: Some(String::from("abcd-4321-wxyz")),
+        };
+        let second_key = KeyListEntry {
+            key_arn: Some(String::from(
+                "arn:aws:kms:us-east-1:123456789:key/efgh-8765-stuv",
+            )),
+            key_id: Some(String::from("efgh-8765-stuv")),
+        };
+        mock_key_list.push(first_key);
+        mock_key_list.push(second_key);
+        assert_eq!(
+            parse_key_list_entries(mock_key_list),
+            vec![
+                "arn:aws:kms:us-east-1:123456789:key/abcd-4321-wxyz|abcd-4321-wxyz",
+                "arn:aws:kms:us-east-1:123456789:key/efgh-8765-stuv|efgh-8765-stuv"
+            ]
+        );
     }
 }
