8f2f83fc8ac4295b94e73d05a33b7b1213c5d38e
Jeff Rade <jeffrade@gmail.com>
2020-12-13 16:08:48 -0600

    Update example app and README with describe-key API call

--- README.md
+++ README.md
@@ -12,4 +12,5 @@ See [examples](https://github.com/jeffrade/kms_rs/blob/master/examples/src/main.
 
 #### As functionality is added, it will be listed below.
 
- - Retrieve a list of all CMK's (Customer Master Keys) in region us-east-1
+ - [Retrieve a list](https://docs.aws.amazon.com/cli/latest/reference/kms/list-keys.html) of all CMK's (Customer Master Keys) in region us-east-1
+ - [Describe a single key](https://docs.aws.amazon.com/cli/latest/reference/kms/describe-key.html) given a key-id
--- examples/src/main.rs
+++ examples/src/main.rs
@@ -1,6 +1,22 @@
+use std::env;
+
 extern crate kms_rs;
 
 fn main() {
-    let keys: Vec<String> = kms_rs::list_keys();
-    println!("{:?}", keys);
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        let keys: Vec<String> = kms_rs::list_keys();
+        println!("{:?}", keys);
+    } else {
+        let cmd: &str = &args[1];
+        match cmd {
+            "describe-key" => {
+                let key_id = &args[2];
+                let key: String = kms_rs::describe_key(key_id);
+                println!("{}", key);
+            }
+            _ => println!("{} command not supported!", cmd),
+        }
+    }
 }
