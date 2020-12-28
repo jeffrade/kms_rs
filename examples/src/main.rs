60c17b096a489e6722f3a27d14b962edf2bf9981
Jeff Rade <jeffrade@gmail.com>
2020-12-28 17:06:48 -0600

    Use clap crate for improved CLI tool and usage in /examples

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -1,22 +1,34 @@
-use std::env;
-
+extern crate clap;
 extern crate kms_rs;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let matches = clap::App::new("KmsRsExample")
+        .version("0.1.0")
+        .author("Jeff Rade <jeffrade@gmail.com>")
+        .about("Example command line tool showing usage of kms_rs")
+        .subcommand(
+            clap::SubCommand::with_name("list-keys")
+                .about("Gets a list of all customer master keys (CMKs) in the caller's AWS account and Region.")
+        )
+        .subcommand(
+            clap::SubCommand::with_name("describe-key")
+                .about("Provides detailed information about a customer master key (CMK).")
+                .arg_from_usage("--key-id=[KEYID] 'metadata for a given key-id'")
+        )
+        .get_matches();
 
-    if args.len() < 2 {
+    if matches.subcommand_matches("list-keys").is_some() {
         let keys: serde_json::value::Value = kms_rs::list_keys();
         println!("{}", keys.to_string());
-    } else {
-        let cmd: &str = &args[1];
-        match cmd {
-            "describe-key" => {
-                let key_id = &args[2];
-                let key: serde_json::value::Value = kms_rs::describe_key(key_id);
-                println!("{}", key.to_string());
-            }
-            _ => println!("{} command not supported!", cmd),
+    } else if let Some(matches) = matches.subcommand_matches("describe-key") {
+        if matches.is_present("key-id") {
+            let key_id: &str = matches.value_of("key-id").unwrap();
+            let key: serde_json::value::Value = kms_rs::describe_key(key_id);
+            println!("{}", key.to_string());
+        } else {
+            println!("You must provide the key-id arg!");
         }
+    } else {
+        println!("You must pass a valid command!");
     }
 }
