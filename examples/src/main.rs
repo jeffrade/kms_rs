166293d7c448172ec5ebf5eae7bcb7319a75a258
Jeff Rade <jeffrade@gmail.com>
2020-12-28 14:17:55 -0600

    Use serde_json Value to return responses in JSON format

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -6,15 +6,15 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
-        let keys: Vec<String> = kms_rs::list_keys();
-        println!("{:?}", keys);
+        let keys: serde_json::value::Value = kms_rs::list_keys();
+        println!("{}", keys.to_string());
     } else {
         let cmd: &str = &args[1];
         match cmd {
             "describe-key" => {
                 let key_id = &args[2];
-                let key: String = kms_rs::describe_key(key_id);
-                println!("{}", key);
+                let key: serde_json::value::Value = kms_rs::describe_key(key_id);
+                println!("{}", key.to_string());
             }
             _ => println!("{} command not supported!", cmd),
         }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 //! A crate for using AWS KMS
+#![allow(non_snake_case)]
 
 extern crate rusoto_core;
 
@@ -7,26 +8,43 @@ use rusoto_kms::{
     DescribeKeyRequest, KeyListEntry, KeyMetadata, Kms, KmsClient, ListKeysRequest,
     ListKeysResponse,
 };
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+use serde_json::value::Value;
 use tokio::runtime::Runtime;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct KmsRsKey {
+    KeyId: String,
+    KeyArn: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct KmsRsKeyMetadata {
+    KeyId: String,
+    Arn: String,
+    Description: String,
+    Enabled: bool,
+}
 
 fn get_client() -> KmsClient {
     KmsClient::new(Region::UsEast1)
 }
 
 /// Gets the list of all Customer Master Keys (CMKs) in current AWS account (defaults to us-east-1).
-pub fn list_keys() -> Vec<String> {
+pub fn list_keys() -> Value {
     Runtime::new()
         .expect("Failed to create Tokio runtime")
         .block_on(get_keys(get_client()))
 }
 
-pub fn describe_key(key_id: &str) -> String {
+pub fn describe_key(key_id: &str) -> Value {
     Runtime::new()
         .expect("Failed to create Tokio runtime")
         .block_on(get_key(get_client(), key_id))
 }
 
-async fn get_key(client: KmsClient, key_id: &str) -> String {
+async fn get_key(client: KmsClient, key_id: &str) -> Value {
     let request = DescribeKeyRequest {
         grant_tokens: None,
         key_id: key_id.to_string(),
@@ -36,39 +54,38 @@ async fn get_key(client: KmsClient, key_id: &str) -> String {
 
     match result {
         Ok(response) => parse_key_metadata(response.key_metadata.unwrap_or_default()),
-        Err(value) => value.to_string(),
+        Err(value) => json!(value.to_string()),
     }
 }
 
-async fn get_keys(client: KmsClient) -> Vec<String> {
+async fn get_keys(client: KmsClient) -> Value {
     let request = ListKeysRequest::default();
 
     let response: ListKeysResponse = client.list_keys(request).await.unwrap();
     parse_key_list_entries(response.keys.unwrap_or_default())
 }
 
-fn parse_key_list_entries(key_list: Vec<KeyListEntry>) -> Vec<String> {
-    let mut keys_str: Vec<String> = Vec::new();
+fn parse_key_list_entries(key_list: Vec<KeyListEntry>) -> Value {
+    let mut keys_json: Vec<KmsRsKey> = Vec::new();
+
     for key in key_list {
-        let mut key_str: String = String::from(&key.key_arn.unwrap_or_default());
-        key_str.push('|');
-        key_str.push_str(&key.key_id.unwrap_or_default());
-        keys_str.push(key_str);
+        keys_json.push(KmsRsKey {
+            KeyId: key.key_id.unwrap_or_default(),
+            KeyArn: key.key_arn.unwrap_or_default(),
+        });
     }
 
-    keys_str
+    json!({ "Keys": &keys_json })
 }
 
-fn parse_key_metadata(metatdata: KeyMetadata) -> String {
-    let mut key_str = String::new();
-    key_str.push_str(&metatdata.key_id);
-    key_str.push('|');
-    key_str.push_str(&metatdata.arn.unwrap_or_default());
-    key_str.push('|');
-    key_str.push_str(&metatdata.description.unwrap_or_default());
-    key_str.push('|');
-    key_str.push_str(&metatdata.enabled.unwrap_or_default().to_string());
-    key_str
+fn parse_key_metadata(metatdata: KeyMetadata) -> Value {
+    let key_metadata = KmsRsKeyMetadata {
+        KeyId: metatdata.key_id,
+        Arn: metatdata.arn.unwrap_or_default(),
+        Description: metatdata.description.unwrap_or_default(),
+        Enabled: metatdata.enabled.unwrap_or_default(),
+    };
+    json!(&key_metadata)
 }
 
 #[cfg(test)]
@@ -92,12 +109,20 @@ mod tests {
         };
         mock_key_list.push(first_key);
         mock_key_list.push(second_key);
-        assert_eq!(
-            parse_key_list_entries(mock_key_list),
-            vec![
-                "arn:aws:kms:us-east-1:123456789:key/abcd-4321-wxyz|abcd-4321-wxyz",
-                "arn:aws:kms:us-east-1:123456789:key/efgh-8765-stuv|efgh-8765-stuv"
+        let actual_output = parse_key_list_entries(mock_key_list);
+        let expected_output = json!({ "Keys":
+            [
+                {
+                    "KeyId": "abcd-4321-wxyz",
+                    "KeyArn": "arn:aws:kms:us-east-1:123456789:key/abcd-4321-wxyz"
+                },
+                {
+                    "KeyId": "efgh-8765-stuv",
+                    "KeyArn": "arn:aws:kms:us-east-1:123456789:key/efgh-8765-stuv"
+                }
             ]
-        );
+
+        });
+        assert_eq!(actual_output, expected_output);
     }
 }
